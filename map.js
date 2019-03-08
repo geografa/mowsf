8baf6706a06d98dcbabf0d3b4d5bbf44b2e0f468
Rafa Gutierrez <rafa@mapbox.com>
2019-03-08 15:20:31 -0800

    adds button/instructions

--- map.js
+++ map.js
@@ -7,55 +7,42 @@ var map = new mapboxgl.Map({
 });
 
 map.on('load', function () {
-
     map.addLayer({
-        "id": "locations",
-        "type": "symbol",
-        "source": {
-            type: 'vector',
-            url: 'mapbox://grafa.cjsz5wor900fi45muprgr1x9s-5fcb6'
-        },
-        "source-layer": "mowsf",
-        "layout": {
-			"icon-image": "monument-15",
-			"text-field": "{name}",
-			"text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
-			"text-offset": [0, 0.6],
-			"text-anchor": "top"
-		}
-	});
+			"id": "locations",
+			"type": "symbol",
+			"source": {
+					type: 'vector',
+					url: 'mapbox://grafa.cjsz5wor900fi45muprgr1x9s-5fcb6'
+			},
+			"source-layer": "mowsf",
+			"layout": {
+				"icon-image": "circle-15",
+				"text-field": "{name}",
+				"text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+				"text-offset": [0, 0.6],
+				"text-anchor": "top"
+			}
+		});
 	
-	// When a click event occurs on a feature in the locations layer, open a popup at the
-    // location of the feature, with description HTML from its properties.
-    map.on('click', 'locations', function (e) {
+	// When a click event occurs on a feature in the locations layer,paas the
+	// location of the feature, with description HTML from its properties.
+	map.on('click', 'locations', function (e) {
 		var coordinates = e.features[0].geometry.coordinates.slice();
-        var id = e.features[0].properties.id;
-        var description = e.features[0].properties.name;
-
-        // Ensure that if the map is zoomed out such that multiple
-        // copies of the feature are visible, the popup appears
-        // over the copy being pointed to.
-        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-		}
-
-        new mapboxgl.Popup()
-            .setLngLat(coordinates)
-            .setHTML(description)
-			.addTo(map);
-		
+		var id = e.features[0].properties.id;
+		var description = e.features[0].properties.name;
 		renderListings(id,description,coordinates)
+		document.getElementById("calcButton").style.display='block';
 	});
 
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'locations', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+	// Change the cursor to a pointer when the mouse is over the places layer.
+	map.on('mouseenter', 'locations', function () {
+			map.getCanvas().style.cursor = 'pointer';
+	});
 
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'locations', function () {
-        map.getCanvas().style.cursor = '';
-    });
+	// Change it back to a pointer when it leaves.
+	map.on('mouseleave', 'locations', function () {
+			map.getCanvas().style.cursor = '';
+	});
 });
 
 function renderListings(id,description,coordinates) {
@@ -63,9 +50,9 @@ function renderListings(id,description,coordinates) {
 	
 	node.innerHTML = 	'<div class="list-group-item">' + 
 						'<span class="badge">' + id + '</span>' + 
-						'<span>' + description + '</span>' + 
-						'<span class="glyphicon glyphicon-move" aria-hidden="true">' + 
-						'</span><div id="feature-listing" class="listing">' + coordinates + '</div></div>';
+						'<span class="glyphicon glyphicon-move" aria-hidden="true"></span>' + 
+						'<span class="listing">' + description + '</span>' + 
+						'<div id="feature-listing" class="listing">' + coordinates + '</div></div>';
 	
 	document.getElementById('listWithHandle').appendChild(node);
 }
@@ -93,9 +80,20 @@ function drawRoute(coords) {
 		console.log(url);	
 		var coords = jsonResponse.trips[0].geometry;
 		addRoute(coords); // add the route to the map
+		var tripInstructions = [];
+		var legs = jsonResponse.trips[0].legs;
+		
+		for(var i = 0; i < legs.length; i++) {
+			var step = legs[i];
+			for(var j = 0; j < step.steps.length; j++) {
+				tripInstructions.push('<li>' + step.steps[j].maneuver.instruction + '</li>');
+				results.innerHTML = tripInstructions.join('');
+			}
+		}
 	};
 	req.send();
 }
+
 
 // adds the route as a layer on the map
 function addRoute (coords) {
@@ -104,37 +102,26 @@ function addRoute (coords) {
 		map.getSource('route').setData(coords);
 	} else{
 	  map.addLayer({
-		"id": "route",
-		"type": "line",
-		"source": {
-		  "type": "geojson",
-		  "data": {
-			"type": "Feature",
-			"properties": {},
-			"geometry": coords
-		  }
-		},
-		"layout": {
-		  "line-join": "round",
-		  "line-cap": "round"
-		},
-		"paint": {
-		  "line-color": "#3b9ddd",
-		  "line-width": 8,
-		  "line-opacity": 0.8
-		}
+			"id": "route",
+			"type": "line",
+			"source": {
+				"type": "geojson",
+				"data": {
+					"type": "Feature",
+					"properties": {},
+					"geometry": coords
+				}
+			},
+			"layout": {
+				"line-join": "round",
+				"line-cap": "round"
+			},
+			"paint": {
+				"line-color": "#3b9ddd",
+				"line-width": 8,
+				"line-opacity": 0.8
+			}
 	  });
 	};
-  }
-  
-  // remove the layer if it exists
-  function removeRoute () {
-	if (map.getSource('route')) {
-	  map.removeLayer('route');
-	  map.removeSource('route');
-	//   document.getElementById('calculated-line').innerHTML = '';
-	} else  {
-	  return;
-	}
-  }
+}
   
